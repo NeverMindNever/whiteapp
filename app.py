5de1c919db098321d3ba20d5cef35e95cdf0dcf1
A. T <alaa@live.cl>
2022-04-09 12:48:29 +0200

    Creating a simple app with two functions multiply and divide, and some simple unit test

---
+++ app.py
@@ -0,0 +1,10 @@
+# Author : Alaa TOUNSADI
+
+def multiply(a, b):
+    # this function return the multiplication of the two parameters
+    return a * b
+
+
+def divide(a, b):
+    # this function return the division of the first parameter with the second
+    return a / b
